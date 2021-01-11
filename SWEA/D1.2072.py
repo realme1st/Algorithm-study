e89b9073bdb3bd6aa800c685c0d65b7352e7cd69
realme1st <62661142+realme1st@users.noreply.github.com>
2021-01-12 05:28:54 +0900

    Create D1.2072.py

---
+++ SWEA/D1.2072.py
@@ -0,0 +1,8 @@
+t = int(input())
+
+for i in range(t):
+    numbers = list(map(int,input().split()))
+    
+    odd_nums = [num for num in numbers if num%2==1]
+    
+    print("#{} {}".format(i+1,sum(odd_nums)))
