c841da2a423f37317db35cfb1ef490bcf9670421
realme1st <62661142+realme1st@users.noreply.github.com>
2021-01-12 01:20:09 +0900

    11047번 : 동전 0
    11047번 동전 0 번 소스코드

---
+++ Baekjoon/그리디/11047번 동전 0.py
@@ -0,0 +1,15 @@
+n,k = map(int,input().split())
+num=0
+money = []
+for i in range(n):
+    money.append(int(input()))
+
+for i in range(n-1,-1,-1):
+    if k==0:
+        break
+    if money[i]>k:
+        continue
+    num+=k//money[i]
+    k%=money[i]
+    
+print(num)
