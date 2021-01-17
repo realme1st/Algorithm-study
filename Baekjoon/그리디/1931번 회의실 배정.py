c4c122a91e36f504de735005f58aa1e5b600542b
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2021-01-18 04:29:32 +0900

    Create 1931번 회의실 배정.py

---
+++ Baekjoon/그리디/1931번 회의실 배정.py
@@ -0,0 +1,20 @@
+n = int(input())
+arr = []
+
+for i in range(n):
+    a,b = map(int,input().split())
+    arr.append((a,b))
+
+arr.sort(key=lambda x: (x[1],x[0]))
+
+count=1
+
+prev = arr[0][1]
+
+print(prev)
+for sch in arr[1:]:
+    if prev <=sch[0]:
+        prev =sch[1]
+        count+=1
+
+print(count)
