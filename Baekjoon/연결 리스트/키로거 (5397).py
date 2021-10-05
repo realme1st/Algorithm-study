340235ee202a5aa196a2fc84ceedc9a8710b0090
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2021-10-05 22:31:14 +0900

    Update 키로거 (5397).py

--- Baekjoon/연결 리스트/키로거 (5397).py
+++ Baekjoon/연결 리스트/키로거 (5397).py
@@ -1,21 +1,21 @@
 import sys
-
-n= int(sys.stdin.readline())
+n = int(sys.stdin.readline())
 
 for _ in range(n):
-    stack_1=[]
-    stack_2=[]
-    l=list(sys.stdin.readline().rstrip())
-    for command in l:
-        if command == '<' and stack_1:
-            stack_2.append(stack_1.pop())
-        elif command=='<' and not stack_1:
-            continue
-        elif command =='>' and stack_2:
-            stack_1.append(stack_2.pop())
-        elif command =='-' and stack_1:
-            stack_1.pop()
+    left_stack = []
+    right_stack = []
+    data = list(sys.stdin.readline().rstrip())
+    for i in data:
+        if i == '-':
+            if left_stack:
+                left_stack.pop()
+        elif i =='<':
+            if left_stack:
+                right_stack.append(left_stack.pop())
+        elif i =='>':
+            if right_stack:
+                left_stack.append(right_stack.pop())
         else:
-            stack_1.append(command)
-
-    print("".join(stack_1+list(reversed(stack_2))))
+            left_stack.append(i)
+    
+    print(''.join(left_stack+list(reversed(right_stack))))
