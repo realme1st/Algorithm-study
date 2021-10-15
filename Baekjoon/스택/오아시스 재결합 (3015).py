537cb50ceb06bf3c9853498c389733ef0c7b3921
SeongMin Lee <62661142+realme1st@users.noreply.github.com>
2021-10-16 02:17:13 +0900

    Update 오아시스 재결합 (3015).py

--- Baekjoon/스택/오아시스 재결합 (3015).py
+++ Baekjoon/스택/오아시스 재결합 (3015).py
@@ -1,26 +1,35 @@
-n=int(input())
+import sys
 
-ans=0
-stack=[]
-# stack에 추가 되는 수는 튜플형식(a,b) a는 수를 나타내고 b는 앞자리까지의 영향력을 나타냄
+input = sys.stdin.readline
 
-for _ in range(n):
-    h=int(input())
-    # 스택의 제일 끝자리보다 큰 수가 들어왔을 시, stack[-1] 수의 앞자리 영향력을 더해준다.
-    while stack and stack[-1][0]<h:
-        ans+= stack.pop()[1]
-    
-    if stack and stack[-1][0]==h:
-        cnt =stack.pop()[1]
-        ans+=cnt
-        # len(stack)이 0이 아니라면, stack에 수가 들어왔을 시, stack[-1]과 새로운 수의 관계를 더해준다.
-        if len(stack)!=0:
-            ans+=1
-        stack.append((h,cnt+1))
-    else:
-        # len(stack)이 0이 아니라면, stack에 수가 들어왔을 시, stack[-1]과 새로운 수의 관계를 더해준다.
-        if len(stack)!=0:
-            ans+=1
-        stack.append((h,1))
+N = int(input())
+heights = [int(input()) for _ in range(N)]
 
-print(ans)
+count = 0
+stack = []
+
+for height in heights:
+    nowcount = 1
+    while stack:
+        if height >= stack[-1][0]:
+            count += stack[-1][1]
+			
+            # 현재 스택의 top과 키가 같다면
+            if height == stack[-1][0]:
+            	# 볼 수 있는 사람 수를 더해준다
+                nowcount += stack[-1][1]
+			
+            # 더 이상 stack의 top을 볼 수 있는 사람이 없으므로
+            # 스택에서 버린다.
+            stack.pop()
+
+        else:
+            # 서로 볼 수 있는 쌍
+            count += 1
+            break
+	
+    # 스택에 해당 키를 넣는다
+    # 이때, 현재 사람을 볼 수 있는 카운트를 추가
+    stack.append((height, nowcount))
+
+print(count)
